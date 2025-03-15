91c4c7def592f7be0f6ed90508fcead5eee19195
sharif <shahanur700@gmail.com>
2025-03-15 22:45:23 +0800

    first

--- core/config.py
+++ core/config.py
@@ -2,3 +2,8 @@ import sys
 import os
 CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH", "/usr/local/bin/chromedriver")
 SEEK_EMAIL = "cristianaanna@gmail.com"
+
+load_dotenv()
+
+# Use environment variables
+db_url = os.getenv('DATABASE_URL', 'sqlite:///data/jobs.db')
