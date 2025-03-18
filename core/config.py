b744f1242db22890209ad342545962875fffaa77
sharif <shahanur700@gmail.com>
2025-03-18 13:18:15 +0800

    first

--- core/config.py
+++ core/config.py
@@ -2,11 +2,13 @@ import sys
 import os
 
 from dotenv import load_dotenv
+load_dotenv()
 
 CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH", "/usr/local/bin/chromedriver")
 SEEK_EMAIL = os.getenv('SEEK_EMAIL')
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+CV_TEXT = os.getenv("CV_TEXT")
 
-load_dotenv()
 
 # Use environment variables
 db_url = os.getenv('DATABASE_URL', 'sqlite:///data/jobs.db')
